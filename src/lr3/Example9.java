4b36e1b8a8e511f7781fb4b0c4c821c8393a2729
anton <gladilian@icloud.com>
2024-12-29 14:05:38 +0500

    Lr 3

--- src/lr3/Example9.java
+++ src/lr3/Example9.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Example9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите размер массиваа");
+        System.out.println("Введите размер массива");
         int size = in.nextInt();
         int[] nums = new int[size];
         Random random = new Random();
